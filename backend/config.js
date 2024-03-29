f33552851323f63733c61539a7e6ee6e80f24275
Muhammed Suhaib <suhaib.muhammed2002@gmail.com>
2024-03-29 23:17:14 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://suhaib:adminThe0@bookbank.eqqoyyu.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookBank';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
